b26fc2f6fdde1edf94580beba70355d9b844b7a7
Tomas Vana <creepone@gmail.com>
2014-08-09 21:33:06 +0200

    fixed flicker on now playing screen

--- Player/PLNowPlayingManager.m
+++ Player/PLNowPlayingManager.m
@@ -11,6 +11,9 @@
     PLKVOObserver *_playerObserver;
     PLNotificationObserver *_notificationObserver;
     RACDisposable *_artworkSubscription;
+    
+    PLTrack *_currentTrack;
+    MPMediaItemArtwork *_currentArtwork;
 }
 @end
 
@@ -41,13 +44,15 @@
 
 - (void)updateInfo:(NSString *)reason
 {
-    [_artworkSubscription dispose];
-    
     PLPlayer *player = [PLPlayer sharedPlayer];
     MPNowPlayingInfoCenter *infoCenter = [MPNowPlayingInfoCenter defaultCenter];
     PLTrack *track = [[player currentSong] track];
     
     if (track == nil) {
+        _currentTrack = nil;
+        _currentArtwork = nil;
+        [_artworkSubscription dispose];
+        
         DDLogInfo(@"Updated now playing (%@) to nil", reason);
         infoCenter.nowPlayingInfo = nil;
         return;
@@ -61,16 +66,27 @@
         MPNowPlayingInfoPropertyPlaybackRate: @(player.playbackRate)
     }];
     
+    if (track == _currentTrack) {
+        if (_currentArtwork)
+            info[MPMediaItemPropertyArtwork] = _currentArtwork;
+    }
+    else {
+        _currentTrack = track;
+        _currentArtwork = nil;
+        
+        [_artworkSubscription dispose];
+        _artworkSubscription = [track.largeArtwork subscribeNext:^(UIImage *artwork) {
+            _currentArtwork = artwork ? [[MPMediaItemArtwork alloc] initWithImage:artwork] : nil;
+            
+            if (_currentArtwork != nil && track == _currentTrack) {
+                info[MPMediaItemPropertyArtwork] = _currentArtwork;
+                infoCenter.nowPlayingInfo = info;
+            }
+        }];
+    }
+    
     infoCenter.nowPlayingInfo = info;
-    
     DDLogInfo(@"Updated now playing (%@) = %@", reason, info);
-    
-    _artworkSubscription = [track.largeArtwork subscribeNext:^(UIImage *artwork) {
-        if (artwork != nil) {
-            info[MPMediaItemPropertyArtwork] = [[MPMediaItemArtwork alloc] initWithImage:artwork];
-            infoCenter.nowPlayingInfo = info;
-        }
-    }];
 }
 
 @end
